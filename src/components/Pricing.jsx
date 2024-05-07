3c429945548099d3b7da3c35d6a8619f4f678acc
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-07 15:35:19 -0500

    pricing sect done; bug fixes

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,6 +1,8 @@
 import Section from "./Section";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -29,6 +31,21 @@ const Pricing = () => {
           tag="Get started with Brainwave"
           title="Pay once, use forever"
         />
+
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
       </div>
     </Section>
   );
