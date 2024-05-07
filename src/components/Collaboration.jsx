0822be28b6c3a7854a925fc89fbd6df20716c56a
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-07 10:50:23 -0500

    bug fix in collab jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,3 +1,5 @@
+import { check } from "../assets";
+import { collabContent } from "../constants";
 import Section from "./Section";
 
 const Collaboration = () => {
@@ -5,7 +7,23 @@ const Collaboration = () => {
     <Section crosses>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
-          <h2>AI Chat App for seamless collaboration</h2>
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
+            {collabContent.map((item) => (
+              <li className="mb-3 py-3" key={item.id}>
+                <div className="flex items-center">
+                  <img src={check} width={24} height={24} alt="check" />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
+                  {item.text && (
+                    <p className="body-2 ml-3 text-n-4">{item.text}</p>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </Section>
