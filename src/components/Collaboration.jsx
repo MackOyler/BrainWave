5e29a61841ac46e6f7391d9c5e89c4f77cdeb2fd
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-07 10:30:31 -0500

    more collab sect work

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,15 @@
+import Section from "./Section";
+
 const Collaboration = () => {
   return (
-    <div>Collaboration</div>
-  )
-}
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2>AI Chat App for seamless collaboration</h2>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Collaboration
+export default Collaboration;
